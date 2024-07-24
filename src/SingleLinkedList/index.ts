a7f83eca3a6d7156c589b63ab3de67f750083ce9
Gourav Mukherjee <43634024+iamgouravmukherjee@users.noreply.github.com>
2024-07-24 16:26:40 -0400

    [SingleLinkedList] reversing a list (#3)
    Co-authored-by: iamgouravmukhejee <gouravmukherjee09@gmail.com>

--- src/SingleLinkedList/index.ts
+++ src/SingleLinkedList/index.ts
@@ -5,9 +5,8 @@ const list = new SingleLinkedList();
 // Before appending
 list.traverse();
 
-
-console.log("Appending to the list");
 // Append to the list
+console.log("Appending to the list");
 list.append(3);
 list.append(4);
 list.append(5);
@@ -15,10 +14,22 @@ list.append(5);
 // After appeding
 list.traverse();
 
-console.log("Prepending to the list");
 // Prepending to the list
+console.log("Prepending to the list");
 list.prepend(2);
 list.prepend(1);
 
 // After prepending
+list.traverse();
+
+// Reverse the list
+console.log("Reversing the list");
+list.reverse();
+
+list.traverse();
+
+// Reverse again, should become original
+console.log("Reversing the list");
+list.reverse();
+
 list.traverse();
